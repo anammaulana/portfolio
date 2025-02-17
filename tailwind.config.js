fb5deaffaa43d019e4deace7b66e512c74de6530
anammaulana <133523637+anammaulana@users.noreply.github.com>
2025-02-17 12:02:53 +0700

    redisign to tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    darkMode: 'class', // or 'media' if you want to use media queries
+    content: [
+      './index.html',
+      './main.js',
+    ],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  }
+  
